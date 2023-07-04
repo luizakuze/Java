a6441820716df8d14ac8d31ebfd8c977b700b5cf
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-07-04 20:48:24 -0300

    Add files via upload

---
+++ introdução/Programa1.java
@@ -0,0 +1,8 @@
+package Luiza;
+
+public class Programa1 {
+
+	public static void main(String[] args) {
+		System.out.println("Olá mundo");
+	}
+}
---
+++ introdução/Programa3.java
@@ -0,0 +1,17 @@
+package Luiza;
+
+// O compilador e o bytecode
+public class Programa3 {
+
+	public static void main(String[] args) {
+		// Declarando e inicializando variável
+		int idade = 18;
+		
+		// Imprimindo na tela
+		System.out.println("Eu tenho " + idade + " anos.");
+
+		// javac Programa.java -> Programa.class -> Java Programa
+		// código fonte (Programa.java) -> bytecode (Programa.class) -> executável
+	}
+
+}

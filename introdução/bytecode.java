9e24e2da6c1f2f7bf680d12b27d649d45dcbae50
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-07-06 20:30:34 -0300

    Update and rename Programa3.java to bytecode.java

--- introdução/bytecode.java
+++ introdução/bytecode.java
@@ -4,14 +4,14 @@ package Luiza;
 public class Programa3 {
 
 	public static void main(String[] args) {
-		// Declarando e inicializando vari·vel
+		// Declarando e inicializando vari√°vel
 		int idade = 18;
 		
 		// Imprimindo na tela
 		System.out.println("Eu tenho " + idade + " anos.");
 
 		// javac Programa.java -> Programa.class -> Java Programa
-		// cÛdigo fonte (Programa.java) -> bytecode (Programa.class) -> execut·vel
+		// c√≥digo fonte (Programa.java) -> bytecode (Programa.class) -> execut√°vel
 	}
 
 }

59f9ad89132e15cd5b06fb29b63ce3c0b3572823
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-07-06 20:31:04 -0300

    Update and rename Programa2.java to variaveis.java

---
+++ introdução/variaveis.java
@@ -0,0 +1,24 @@
+package Luiza;
+
+//Variáveis
+public class Programa2 {
+	
+	public static void main(String[] args) {
+		//Declaração de variável
+		int valor;
+		
+		//Declaração de variáveis
+		int num1, num2, num3;
+		
+		//Declarando e inicializando allgumas variáveis
+		int num4, num5 = 10, num6;
+		
+		//Por padrão, os valores d eponto flutuante são double em Java
+		//Requer o 'f' no final do número
+		float x = 12.4f;
+		
+		double y = 12.4;
+		
+	}
+	
+}

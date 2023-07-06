cd6f0499529ce2a2b55165852fdbabb6282de561
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-07-06 20:30:48 -0300

    Update and rename Programa4.java to main.java

---
+++ introdução/main.java
@@ -0,0 +1,13 @@
+package Luiza;
+
+// A função main()
+
+// Linguagem orientada a objetos
+public class Programa4 {
+	// Essa função que torna a classe java em um programa Java. É a int main do C
+	// Recebe como parâmetro um array de strings chamado args
+	public static void main(String[] args) {
+		System.out.println("Texto");
+		System.out.println(args); // endereço de memória
+	}
+}

6cb61cec32c983113c34e0d113b5e0e5e767e26d
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-07-10 15:10:52 -0300

    Update Matrizes1.java

--- vetores e matrizes/Matrizes1.java
+++ vetores e matrizes/Matrizes1.java
@@ -15,20 +15,20 @@ package Luiza;
 
 public class Matrizes1 {	
 	public static void main(String[] args) {
-		// Declaração
+		// DeclaraÃ§Ã£o
 		int outros_numeros[][];
 		
-		// Declaração e definição de tamanho da matriz
+		// DeclaraÃ§Ã£o e definiÃ§Ã£o de tamanho da matriz
 		int numeros[][] = new int[3][3];
 
 		// Declarar, definir o tamanho e inicializar
-		int main_numeros[][] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+		int mainNumeros[][] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
 		
 		// Declarar uma matriz informando somente as linhas
 		int matriz[][] = new int [2][];
 		matriz[0] = new int[5];
 		matriz[1] = new int[3];
 		
-		int nova_matriz[][] = { {1, 2}, {4, 5, 6, 7, 8}, {9, 10, 11} };
+		int novaMatriz[][] = { {1, 2}, {4, 5, 6, 7, 8}, {9, 10, 11} };
 	}
 }

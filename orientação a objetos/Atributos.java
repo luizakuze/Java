230c03ecac08200c9a6022ef35762d5608ca2b74
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-07-11 10:51:48 -0300

    Create Atributos.java

---
+++ orientação a objetos/Atributos.java
@@ -0,0 +1,18 @@
+package Luiza;
+
+// Atributos
+
+/*
+- São as características da classe/molde/modelo de dados:
+- Podemos entender atributos como variáveis da classe;
+- Uma outra forma de nomenclatura para os atributos são estados;
+- Atributos são nomeados em letras minúsculas, sem espaços, sem
+  caracteres especiais e sem acentuação.
+
+ */
+
+public class Produto {
+	String nome;
+	float preco;
+	float desconto;
+}

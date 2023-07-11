78b64b045893e711667d5af681918646a1ab2beb
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-07-11 10:46:24 -0300

    Create Classe.java

---
+++ orientação a objetos/Classe.java
@@ -0,0 +1,20 @@
+package Luiza;
+
+// Classe representa um produto
+
+/*
+- O nome das classes inicia com letra miúscula;
+- O nome não deve conter acentuação, caracteres especiais e espaços;
+- Nas classes java, não existe a implementação da função main()
+- Toda classe Java possui a seguinte forma:
+- O nome da classe Java DEVE ser o mesmo nome do arquivo java;
+- Tudo que estiver dentro das "chaves" {} faz parte da classe.
+
+public class NomeDaClasse {
+
+}
+*/
+
+public class Produto {
+	
+}

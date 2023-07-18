7b51846a301e6a258ac3e9af2885dbd1b6799642
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-07-17 23:06:34 -0300

    Create pacotes.java

---
+++ pacotes/pacotes.java
@@ -0,0 +1,26 @@
+package Luiza;
+
+// Pacotes 
+
+/* 
+- Pacotes servem para organizar o código fonte do projeto;
+- Pacotes indicam onde determinado arquivo do projeto se encontra;
+- Os nomes de pacotes devem ser em letras minúsculas, sem espaço, sem
+  caracteres especiais, sem acentuação;
+- Por convenção, é utilizado nos nomes de pacotes, o domínio da empresa
+criadora do projeto, ao contrário, sendo a última parte do nome
+o especificador do pacote;
+
+- Padrão de Projeto
+- MVC - Model - View - Controller
+*/
+
+
+public class Programa01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

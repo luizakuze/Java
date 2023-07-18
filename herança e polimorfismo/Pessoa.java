c6d510332971c404335831a770035b87eac438be
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-07-18 20:40:56 -0300

    Create Pessoa.java

---
+++ herança e polimorfismo/Pessoa.java
@@ -0,0 +1,47 @@
+package Luiza;
+
+/*
+- Classe base;
+- Classe mãe/pai;
+- Super classe;
+- Classe genérica.
+ */
+
+public class Pessoa {
+	// Privado, encapsular
+	private String nome;
+	private int anoNascimento;
+	private String email;
+	
+	// Construtor 
+	public Pessoa(String nome, int anoNascimento, String email) {
+		this.nome = nome;
+		this.anoNascimento = anoNascimento;
+		this.email = email;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	// setar o valor
+	public void setNome(String Nomes) {
+		this.nome = nome;
+	}
+	
+	public int anoNascimento() {
+		return this.anoNascimento;
+	}
+	
+	public void setAnoNascimento(int anoNascimento) {
+		this.anoNascimento = anoNascimento;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}

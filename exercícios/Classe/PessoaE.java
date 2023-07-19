f93e4e8d311e182dd494b76c109be25e3ff44065
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-07-19 16:49:48 -0300

    Create PessoaE.java

---
+++ exercícios/Classe/PessoaE.java
@@ -0,0 +1,50 @@
+package Luiza;
+
+// Declaração da classe
+public class PessoaE {
+	
+	// Atributos
+	private String nome;
+	private String endereco;
+	private String telefone;
+	
+	// Construtor vazio
+	public PessoaE (String nome, String endereco, String telefone) {
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefone = telefone;
+	}
+		
+	// Métodos
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEndereco() {
+		return this.endereco;
+	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	public String getTelefone() {
+		return this.endereco;
+	}
+	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public void imprime() {
+		System.out.println("Nome: " + this.nome);
+		System.out.println("Endereço: " + this.endereco);
+		System.out.println("Telefone: " + this.telefone);
+	}
+
+}
